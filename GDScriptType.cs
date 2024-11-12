471825fa0039c6dfa6a06cb02fff9d09269074d2
Patty <94728986+PattyHoswell@users.noreply.github.com>
2024-11-12 18:18:16 +0700

    Add C# wrapper

---
+++ GDScriptType.cs
@@ -0,0 +1,26 @@
+using Godot;
+using Godot.Collections;
+
+public static class GDScriptType
+{
+    private static GDScript Internal_Script;
+
+    static GDScriptType()
+    {
+        Internal_Script = GD.Load<GDScript>("res://Type.gd");
+        if (Internal_Script is null)
+        {
+            GD.PushError("Unable to get Type.gd, please make sure the path is correct");
+        }
+    }
+
+    public static Array IsExtendingFrom(GodotObject obj, bool readable_names = false)
+    {
+        return Internal_Script.Call("extending_from", obj, readable_names).AsGodotArray();
+    }
+
+    public static bool IsInheritFrom(string child, string parent, bool check_cached_result = true)
+    {
+        return Internal_Script.Call("inherit_from", child, parent, check_cached_result).AsBool();
+    }
+}
--- TestChildCSharp.cs
+++ TestChildCSharp.cs
@@ -3,4 +3,9 @@ using Godot;
 [GlobalClass]
 public partial class TestChildCSharp : TestParentCSharp
 {
+    public override void _Ready()
+    {
+        if (GDScriptType.IsInheritFrom("TestTypeMatcher", "TestParent"))
+            GD.Print("TestTypeMatcher inherit TestParent (C# check)");
+    }
 }
